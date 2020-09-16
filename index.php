4129b981df3f9c24f013c511eeead4a47c3d0343
phsangit <phsang49@gmail.com>
2020-09-16 20:53:44 +0700

    Test commit git

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>login</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<div class="form">
+<?php
+    include("header.php");
+?>
+</div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,141 @@
+<?php 
+    include("Connect.php");
+    session_start();
+    $LoginErr ="";
+    if (isset($_POST['username'])){
+        // $username = $_POST['username'];
+        $username = stripslashes($_REQUEST['username']);
+        $username = mysqli_real_escape_string($conn,$username);
+        // $password = $_POST['password'];
+        $password = stripslashes($_REQUEST['password']);
+        $password = mysqli_real_escape_string($conn,$password);
+        $query = "SELECT * FROM `users` WHERE username='$username' and password='$password'";
+        $result = mysqli_query($conn,$query) or die(mysql_error());
+        $rows = mysqli_num_rows($result);
+            if($rows==1){
+                $_SESSION['username'] = $username;
+                header("Location: index.php");
+                $LoginErr= $username;
+            }
+            else{
+                // $LoginErr ="Sai Tài khoản hoặc Mật Khẩu!";
+                // $LoginErr= $username;
+            }
+        }
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Login | User SIT</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description">
+        <meta content="Themesbrand" name="author">
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets\images\favicon.ico">
+        <!-- Bootstrap Css -->
+        <link href="assets\css\bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css">
+        <!-- Icons Css -->
+        <link href="assets\css\icons.min.css" rel="stylesheet" type="text/css">
+        <!-- App Css-->
+        <link href="assets\css\app.min.css" id="app-style" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div class="home-btn d-none d-sm-block">
+            <a href="index.html" class="text-dark"><i class="fas fa-home h2"></i></a>
+        </div>
+        <div class="account-pages my-5 pt-sm-5">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8 col-lg-6 col-xl-5">
+                        <div class="card overflow-hidden">
+                            <div class="bg-soft-primary">
+                                <div class="row">
+                                    <div class="col-7">
+                                        <div class="text-primary p-4">
+                                            <h5 class="text-primary">Hệ Thống User SIT.</h5>
+                                            <p>Sign in to continue to SIT.</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-5 align-self-end">
+                                        <img src="assets\images\profile-img.png" alt="" class="img-fluid">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-body pt-0"> 
+                                <div>
+                                    <a href="index.html">
+                                        <div class="avatar-md profile-user-wid mb-4">
+                                            <span class="avatar-title rounded-circle bg-light">
+                                                <img src="assets\images\logo.svg" alt="" class="rounded-circle" height="34">
+                                            </span>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="p-2">
+                                    <form class="form-horizontal" action="" method="post" name="login">
+                                        <i class="text-danger mb-5"><?php echo $LoginErr;?></i>
+                                        <div class="form-group mt-1">
+                                            <label for="username">Tài Khoản</label>
+                                            <input type="text" class="form-control" name="username" id="username" placeholder="Enter username">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="userpassword">Mật Khẩu</label>
+                                            <input type="password" class="form-control" name="password" id="userpassword" placeholder="Enter password">
+                                        </div>
+                                        <div class="custom-control custom-checkbox">
+                                            <input type="checkbox" class="custom-control-input" id="customControlInline">
+                                            <label class="custom-control-label" for="customControlInline">Ghi nhớ trạng thái đăng nhập</label>
+                                        </div>
+                                        
+                                        <div class="mt-3">
+                                            <button class="btn btn-primary btn-block waves-effect waves-light" type="submit">Đăng Nhập</button>
+                                        </div>
+                                        <!-- <div class="mt-4 text-center">
+                                            <h5 class="font-size-14 mb-3">Sign in with</h5>
+                                            <ul class="list-inline">
+                                                <li class="list-inline-item">
+                                                    <a href="javascript::void()" class="social-list-item bg-primary text-white border-primary">
+                                                        <i class="mdi mdi-facebook"></i>
+                                                    </a>
+                                                </li>
+                                                <li class="list-inline-item">
+                                                    <a href="javascript::void()" class="social-list-item bg-info text-white border-info">
+                                                        <i class="mdi mdi-twitter"></i>
+                                                    </a>
+                                                </li>
+                                                <li class="list-inline-item">
+                                                    <a href="javascript::void()" class="social-list-item bg-danger text-white border-danger">
+                                                        <i class="mdi mdi-google"></i>
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </div> -->
+                                        <div class="mt-4 text-center">
+                                            <a href="auth-recoverpw.html" class="text-muted"><i class="mdi mdi-lock mr-1"></i> Bạn Quên Mật Khẩu</a>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mt-5 text-center">
+                            <div>
+                                <p>Don't have an account ? <a href="auth-register.html" class="font-weight-medium text-primary"> Signup now </a> </p>
+                                <p>© <?php echo Date("Y");?> SIT. All Rights Reserved <i class="mdi mdi-heart text-danger"></i></p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- JAVASCRIPT -->
+        <script src="assets\libs\jquery\jquery.min.js"></script>
+        <script src="assets\libs\bootstrap\js\bootstrap.bundle.min.js"></script>
+        <script src="assets\libs\metismenu\metisMenu.min.js"></script>
+        <script src="assets\libs\simplebar\simplebar.min.js"></script>
+        <script src="assets\libs\node-waves\waves.min.js"></script>
+        <!-- App js -->
+        <script src="assets\js\app.js"></script>
+    </body>
+</html>
